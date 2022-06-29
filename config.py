3287568c17878aa6758983434036ad55ab39a08e
Aaryan <108219888+Yoriichi-sama@users.noreply.github.com>
2022-06-29 17:29:39 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,6 +5,6 @@
 
 """ANIME DL TELEGRAM BOT CREDENTIALS"""
 
-api_id = ""
-api_hash = ""
-bot_token = ""
+api_id = "16812684"
+api_hash = "ac3c705573aca82dd6b2fbc402e7797e"
+bot_token = "5493983821:AAFj3X2D1CF7OxlJOpWrliH4id1DXA28lrk"
